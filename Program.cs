8ef8a198316d53b11317428655e64bcb33107f33
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-02-25 20:39:12 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberStrokes;
+
+            Console.WriteLine("Введите количество ударов по боссу");
+            numberStrokes = Convert.ToInt32(Console.ReadLine());
+            while  (numberStrokes --> 0 ) 
+            {
+                Console.WriteLine(" Вы нанесли ударов ");
+            }
+        }
+    }
+}
